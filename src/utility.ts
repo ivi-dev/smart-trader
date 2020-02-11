044c56f78cc141d12236b63559cfbbd97c1f852a
Iliyan Videv <videviliyan@gmail.com>
2020-02-11 13:27:57 +0200

    Added two new methods for date/time rendering

--- src/utility.ts
+++ src/utility.ts
@@ -12,3 +12,23 @@ export const formatDate = (date: Date) => {
         minute = date.getMinutes();
     return `${year} ${months[month]} ${date_} ${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}`;
 }
+
+export const fullDate = (date: Date, separator: string = '/') => {
+    const hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getMinutes();
+    const hours_ = hours < 10 ? `0${hours}` : hours, minutes_ = minutes < 10 ? `0${minutes}` : minutes, seconds_ = seconds < 10 ? `0${seconds}` : seconds;
+    return `${date.getFullYear()}${separator}${months[date.getMonth()]}${separator}${date.getDate()} ${hours_}:${minutes_}:${seconds_}`;
+}
+
+export const shortDate = (date: Date, separator: string = '/') => {
+    return `${date.getFullYear()}${separator}${months[date.getMonth()]}${separator}${date.getDate()}`;
+}
+
+export const time = (date: Date, separator: string = ':') => {
+    const hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getMinutes();
+    const hours_ = hours < 10 ? `0${hours}` : hours, minutes_ = minutes < 10 ? `0${minutes}` : minutes, seconds_ = seconds < 10 ? `0${seconds}` : seconds;
+    return `${hours_}${separator}${minutes_}${separator}${seconds_}`;
+}
