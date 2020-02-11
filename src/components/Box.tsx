f749ae3fe1956641999b2eceecede5072f20da72
Iliyan Videv <videviliyan@gmail.com>
2020-02-11 13:28:16 +0200

    Implemented sticky table headers

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -27,14 +27,12 @@ const Box: BoxComponent = (prop: BoxProp) => {
             content = <div className="empty-label text-muted text-center col-12 mt-5">No data yet.</div>
         } else {
             content = <div className="row no-gutters content">
-                         <div className="col-12 table-headers row no-gutters position-absolute">
-                            {prop.tableData?.headers.map((header, index) => 
-                            <div key={index} className="col text-center">
-                                {header.content}
-                            </div>)}
-                         </div>
                          <div className="scroll-area col-12">
-                            <table id="order-history-table" className="col-12 mt-4 table table-borderless table-hover">
+                            <table id="order-history-table" className="col-12 table table-borderless table-hover">
+                                <thead>
+                                    {prop.tableData?.headers.map((header, index) => 
+                                    <th>{header.content}</th>)}
+                                </thead>
                                <tbody>
                                    {prop.tableData?.rows.map((row, index) => 
                                     <tr key={index}>
@@ -54,9 +52,9 @@ const Box: BoxComponent = (prop: BoxProp) => {
                 <div className="empty-label text-muted text-center col-12 mt-5">No data yet.</div>
             </div>
         } else {
-            content = <div className="scroll-area mt-2">
+            content = <div className="scroll-area">
                 {prop.listData.items.map((item, index) => 
-                <div key={index} className="row data-row no-gutters px-3 py-1 border-bottom">
+                <div key={index} className="row data-row no-gutters px-3 py-2 border-bottom">
                     <div className="row no-gutters col-12">
                         {item.main}
                     </div>
@@ -90,7 +88,7 @@ const Box: BoxComponent = (prop: BoxProp) => {
         }
     }
     return (
-        <div className={`box col mr-3 mb-2 mt-1 shadow pb-1 rounded ${prop.selected ? 'selected' : null}`} onClick={() => prop.dispatch(actions.selectBox(prop.id))} tabIndex={prop.id} onKeyDown={(e) => handleKeyDown(e.keyCode)}>
+        <div className={`box col mr-3 mt-1 shadow pb-1 rounded ${prop.selected ? 'selected' : null}`} onClick={() => prop.dispatch(actions.selectBox(prop.id))} tabIndex={prop.id} onKeyDown={(e) => handleKeyDown(e.keyCode)}>
             <div className="row no-gutters header p-2 pl-3 align-items-center">
                 {prop.title}
                 <i className="fas fa-times px-2 py-1 ml-auto rounded" onClick={() => {prop.dispatch(actions.removeBox(prop.id))}}></i>
