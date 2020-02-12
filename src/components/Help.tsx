2804a1239083af6638a5efbc2f6ada24c49079f6
Iliyan Videv <videviliyan@gmail.com>
2020-02-12 20:56:07 +0200

    Removed the Esc key support for help

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -22,12 +22,8 @@ const Help = (prop: HelpProp) => {
             }
         } 
     }
-    const ref: RefObject<HTMLDivElement> = React.createRef();
-    useEffect(() => {
-        (ref.current as HTMLDivElement).focus();
-    });
     return (
-        <div className={`help position-fixed shadow rounded p-4 ${prop.visible ? 'slid-down' : 'slid-up'}`} tabIndex={0} onKeyDown={(e) => handleKeyDown(e.keyCode)} ref={ref}>
+        <div className={`help position-fixed shadow rounded p-4 ${prop.visible ? 'slid-down' : 'slid-up'}`} tabIndex={0} onKeyDown={(e) => handleKeyDown(e.keyCode)}>
         <i className="fas fa-times rounded position-absolute px-2 py-1" onClick={() => { if (prop.dispatch) {prop.dispatch(actions.toggleHelp('close'))}}}></i>
             <div className="row no-gutters mt-2 mb-3 border-bottom sections">
         {prop.sections.map((section, index) => <a key={index} href="/" className={`col-auto rounded-top p-1 px-2 text-reset text-decoration-none section mr-1 border ${prop.active.toLowerCase() === section.name.toString().toLowerCase() ? 'active' : null}`} onClick={(e) => {e.preventDefault(); if(prop.dispatch) {prop.dispatch(actions.setActiveHelpSection(section.name.toString()))}}}>{section.name}</a>)}
