3375efea6bcb786c844850a39720a14cfa3d73b6
Iliyan Videv <videviliyan@gmail.com>
2020-02-12 21:24:20 +0200

    Added a handleSelect property to ButtonGroup

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -7,17 +7,24 @@ interface ButtonGroupProp {
     classes?: string,
     btnClasses?: string,
     active?: string,
-    handleSelect?: (value: string) => void
+    handleSelect?: (data: string | number) => void
 }
 
 const ButtonGroup = (prop: ButtonGroupProp) => {
     let i = 0;
+    const handleSelect = (option: Option) => {
+        if (prop.handleSelect) {
+            prop.handleSelect(option.name !== '' ? option.name : option.data!)
+        } else if (option.onClick) {
+            option.onClick(option.name !== '' ? option.name : option.data!)
+        }
+    }
     return (
         <div className={`col-auto btn-group btn-group-sm button-group shadow-sm ${prop.classes}`}role="group" 
         aria-label="Timespan selector">
             {prop.options.map(option => 
                 <button key={i++} type="button" title={option.title}
-                    className={`btn btn-secondary ${prop.active && (option.name === prop.active || option.graphic === prop.active) ? 'active' : null} ${prop.btnClasses}`} onClick={( ) => {if (option.onClick) {option.onClick(option.name !== '' ? option.name : option.data!)}}}>
+                    className={`btn btn-secondary ${prop.active && (option.name === prop.active || option.graphic === prop.active) ? 'active' : null} ${prop.btnClasses}`} onClick={() => handleSelect(option)}>
                     {option.graphic ? <i className={option.graphic}></i> : option.name}
                 </button>)}
         </div>
