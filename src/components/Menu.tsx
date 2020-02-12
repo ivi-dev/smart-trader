ad068254c675c4392815d0017799f6e2a2b1e8c3
Iliyan Videv <videviliyan@gmail.com>
2020-02-12 20:38:26 +0200

    Made the target path of items a valid one

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,7 +12,7 @@ const Menu = (prop: MenuProp) => {
     let i = 0;
     return (
         <div className={`menu position-absolute rounded shadow ${prop.visible ? null : 'd-none'}`}>
-            {prop.items.map(item => <a key={i++} href="#" 
+            {prop.items.map(item => <a key={i++} href="/" 
                 className="row no-gutters py-1 px-3 col-12 text-reset text-decoration-none" onClick={(e) => {if (item.onClick) {e.stopPropagation(); item.onClick(item.name)}}}>
                     {capitalize(item.name.toString())}
                 </a>)}
