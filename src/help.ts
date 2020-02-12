783ad4563eb0c23e589be3e580ad3208ec1b8db2
Iliyan Videv <videviliyan@gmail.com>
2020-02-12 19:56:41 +0200

    Added a help.ts file

---
+++ src/help.ts
@@ -0,0 +1,8 @@
+export const help = {
+    overview: 
+        'Overview.',
+    basicNavigation: 'Basic Navigation',
+    exploringStocks: 'Exploring Stocks',
+    buyingSellinStocks: 'buyingSellingStocks',
+    customizingYourWorkspace: 'Customizing Your Workspace'
+};
