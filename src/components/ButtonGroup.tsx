6561816b183602b30e79bf92e2824064d6a9551e
Iliyan Videv <videviliyan@gmail.com>
2020-02-12 20:15:04 +0200

    Made a property of ButtonGroup optional

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -7,7 +7,7 @@ interface ButtonGroupProp {
     classes?: string,
     btnClasses?: string,
     active?: string,
-    handleSelect: (value: string) => void
+    handleSelect?: (value: string) => void
 }
 
 const ButtonGroup = (prop: ButtonGroupProp) => {
@@ -17,7 +17,7 @@ const ButtonGroup = (prop: ButtonGroupProp) => {
         aria-label="Timespan selector">
             {prop.options.map(option => 
                 <button key={i++} type="button" title={option.title}
-                    className={`btn btn-secondary ${prop.active && (option.name === prop.active || option.graphic === prop.active) ? 'active' : null} ${prop.btnClasses}`} onClick={(e) => {prop.handleSelect(option.data ? option.data : (e.target as HTMLButtonElement).innerHTML)}}>
+                    className={`btn btn-secondary ${prop.active && (option.name === prop.active || option.graphic === prop.active) ? 'active' : null} ${prop.btnClasses}`} onClick={( ) => {if (option.onClick) {option.onClick('')}}}>
                     {option.graphic ? <i className={option.graphic}></i> : option.name}
                 </button>)}
         </div>
