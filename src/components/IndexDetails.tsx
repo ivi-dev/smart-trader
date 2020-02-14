b40de4612bfde320651fd5dac9b265830e938a43
Iliyan Videv <videviliyan@gmail.com>
2020-02-14 18:17:05 +0200

    Removed some fields from StockDetails

--- src/components/IndexDetails.tsx
+++ src/components/IndexDetails.tsx
@@ -9,16 +9,9 @@ type IndexDetailsProp = {
 const IndexDetails = (prop: IndexDetailsProp) =>
     <div className="col-4 row no-gutters index-details align-items-center">
         <div className="name mr-3 my-2">{prop.data?.name || '---'}</div>
-        <div className="open pr-3 text-center">
-            <div className="title">OPEN</div>
-            <div className="value">{prop.data?.open || 'xxx'}</div>
-        </div>
-        <div className="current pr-3 text-center">
-            <div className="title">CURRENT</div>
-            <div className={`value ${prop.data && (prop.data?.current < 
-                prop.data?.open) ? 'negative' : 'positive'}`}>
-                    {prop.data?.current || 'xxx'}
-            </div>
+        <div className="mr-3 my-2 text-muted">{prop.data?.companyName}</div>
+        <div className={`pr-3 text-center ${prop.data!.open <= prop.data!.current ? 'positive' : 'negative'}`}>
+             {prop.data?.trend}%
         </div>
     </div>
 
