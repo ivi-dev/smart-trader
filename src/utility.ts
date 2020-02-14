30744ed5818a18097c4ae2ba622e3891a0ba410b
Iliyan Videv <videviliyan@gmail.com>
2020-02-14 16:22:41 +0200

    Added an alphabet and digits lists to utility.ts

--- src/utility.ts
+++ src/utility.ts
@@ -1,4 +1,6 @@
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+export const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+export const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 export const capitalize = (word: string) => {
