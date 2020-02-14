9c0b4dede9cbcb278d993e555b463090f3213d1c
Iliyan Videv <videviliyan@gmail.com>
2020-02-14 16:21:29 +0200

    Added a style property to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,12 @@ import './Button.css';
 type ButtonProp = {
     title?: string,
     graphic?: string,
-    onClick: () => void,
-    classes?: string
+    onClick: (value?: any) => void,
+    classes?: string,
+    style?: {}
 }
 
 const Button = (prop: ButtonProp) => 
-    <button className={`btn btn-sm btn-outline-light single ${prop.classes}`} onClick={() => prop.onClick()}>{prop.title ? prop.title : <i className={prop.graphic}></i>}</button>
+    <button className={`btn btn-sm btn-outline-light single ${prop.classes}`} style={prop.style} onClick={() => prop.onClick()}>{prop.title ? prop.title : <i className={prop.graphic}></i>}</button>
 
 export default Button;
