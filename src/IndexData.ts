2bce7369095da528551b950835d054c811b22cc7
Iliyan Videv <videviliyan@gmail.com>
2020-02-14 12:25:33 +0200

    Renamed Index to Stock

--- src/IndexData.ts
+++
@@ -1,5 +0,0 @@
-export default class IndexData {
-    constructor(public id: number, public name: string, public open: number, 
-        public close: number, public high: number, public low: number, 
-        public current: number, public trend: number) {}
-}
