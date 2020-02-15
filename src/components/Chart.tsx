859ba64c703d3a242b694fb7f2b897a61ae597f8
Iliyan Videv <videviliyan@gmail.com>
2020-02-15 14:28:26 +0200

    Added changed properties set of Chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, RefObject } from 'react';
-import ChartData from '../ChartData';
-import { createChart } from 'lightweight-charts';
+import ApexCharts from 'apexcharts';
 import './Chart.css';
-import ButtonGroup from './ButtonGroup';
 import { Action } from '../redux/actions';
-import * as actions from '../redux/actions';
 import StockDetails from './StockDetails';
 import { ChartDescriptor } from '../redux/store';
 
 type ChartProp = {
     data: ChartDescriptor,
+    tracker: WebSocket | number | null,
+    trackerMode: 'live' | 'simulated',
     dispatch: (action: Action) => void
 }
 
@@ -20,24 +19,20 @@ const Chart = (prop: ChartProp) => {
         }
     }
     const chartBox: RefObject<HTMLDivElement> = React.createRef();
+
     useEffect(() => {
-        const chartBoxElement = chartBox.current as HTMLDivElement;
-        cleanUp(chartBoxElement);
-        const chart = createChart(chartBoxElement, Object.assign({
-            width: chartBoxElement.clientWidth, 
-            height: chartBoxElement.clientHeight}, prop.data.options));
-        window.onresize = () => chart.applyOptions({
-            width: chartBoxElement.clientWidth, 
-            height: chartBoxElement.clientHeight});
-        if (prop.data.data) {
-            ChartData.addSeries(chart, prop.data.data.entries, prop.data.stock?.name || '');
-        }
+          cleanUp(chartBox.current as HTMLDivElement);
+          var chart = new ApexCharts(chartBox.current, prop.data.options);
+          chart.render();
     });
     return (
         <section className={`row col-12 no-gutters px-4 chart align-items-start`}>
             <div className="row justify-content-between 
                 align-items-center no-gutters col-12">
-                <StockDetails data={prop.data.stock} />
+                <StockDetails data={prop.data.stock} 
+                              tracker={prop.tracker}
+                              trackerMode={prop.trackerMode}
+                              dispatch={prop.dispatch} />
             </div>
             <div ref={chartBox} className={`row no-gutters col-12 graph border rounded`}></div>
         </section>
