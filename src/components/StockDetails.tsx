ee93f065637f9e02d67787e87e3a37d48560415e
Iliyan Videv <videviliyan@gmail.com>
2020-02-15 14:30:02 +0200

    Added a tracker mode indicator

--- src/components/StockDetails.tsx
+++ src/components/StockDetails.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './StockDetails.css';
 import StockData from '../StockData';
+import Button from './Button';
+import { Action } from '../redux/actions';
+import * as actions from '../redux/actions';
 
 type IndexDetailsProp = {
-    data: StockData | null
+    data: StockData | null,
+    tracker: WebSocket | number | null,
+    trackerMode: 'live' | 'simulated',
+    dispatch?: (action: Action) => void
 }
 
 const StockDetails = (prop: IndexDetailsProp) => {
@@ -13,6 +19,18 @@ const StockDetails = (prop: IndexDetailsProp) => {
             color = 'negative';
         }
     }
+    const handleClick = (altKey: boolean) => {
+        if (prop.dispatch) {
+            if (!altKey) {
+                prop.dispatch(actions.toggleStopTracker());
+            } else {
+                prop.dispatch(actions.setTrackerMode(prop.trackerMode === 'simulated' ? false : true));
+            }
+        }
+    }
+    const title = () => {
+        return !prop.tracker ? 'Resume' : (prop.trackerMode === 'simulated' ? 'Simulation' : 'Live');
+    }
     return (
         <div className="col-4 row no-gutters index-details align-items-center">
             <div className="name mr-3 my-2">{prop.data?.name || '---'}</div>
@@ -20,6 +38,9 @@ const StockDetails = (prop: IndexDetailsProp) => {
             <div className={`pr-3 text-center ${color}`}>
                 {`${prop.data?.trend || '---'}%`}
             </div>
+            {prop.data && <Button title={title()} 
+                                  classes={prop.trackerMode}
+                                  onClick={e => handleClick(e.altKey)} />}
         </div>
     )
 }
