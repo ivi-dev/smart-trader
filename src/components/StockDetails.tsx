4847f13faa6f8f956b11bf0f94242563387c7b91
Iliyan Videv <videviliyan@gmail.com>
2020-02-15 21:21:34 +0200

    Added info to StockDetails

--- src/components/StockDetails.tsx
+++ src/components/StockDetails.tsx
@@ -5,19 +5,23 @@ import Button from './Button';
 import { Action } from '../redux/actions';
 import * as actions from '../redux/actions';
 
-type IndexDetailsProp = {
+type StockDetailsProp = {
     data: StockData | null,
     tracker: WebSocket | number | null,
     trackerMode: 'live' | 'simulated',
     dispatch?: (action: Action) => void
 }
 
-const StockDetails = (prop: IndexDetailsProp) => {
-    let color = 'positive';
+const StockDetails = (prop: StockDetailsProp) => {
+    let current = 0, open = 0, 
+    high = 0, low = 0, trendValue = 0, trendPercentage = 0;
     if (prop.data) {
-        if (prop.data!.open > prop.data!.current) {
-            color = 'negative';
-        }
+        current = prop.data.current;
+        open = prop.data.open;
+        high = prop.data.high;
+        low = prop.data.low;
+        trendValue = Number(Math.abs(prop.data.trend).toFixed(2));
+        trendPercentage = Number(Math.abs(trendValue / current * 100).toFixed(2));
     }
     const handleClick = (altKey: boolean) => {
         if (prop.dispatch) {
@@ -29,14 +33,31 @@ const StockDetails = (prop: IndexDetailsProp) => {
         }
     }
     const title = () => {
-        return !prop.tracker ? 'Resume' : (prop.trackerMode === 'simulated' ? 'Simulation' : 'Live');
+        return !prop.tracker ? 'Paused' : 
+            (prop.trackerMode === 'simulated' ?
+            'Simulation Running...' : 'Live mode Active...');
     }
     return (
-        <div className="col-4 row no-gutters index-details align-items-center">
+        <div className="col-auto row no-gutters stock-details align-items-center">
             <div className="name mr-3 my-2">{prop.data?.name || '---'}</div>
-            <div className="mr-3 my-2 text-muted">{prop.data?.companyName || '---'}</div>
-            <div className={`pr-3 text-center ${color}`}>
-                {`${prop.data?.trend || '---'}%`}
+            <div className="company-name mr-3 my-2 text-muted">{prop.data?.companyName || '---'}</div>
+            <div className="current mr-4 my-2">{current}</div>
+            <div className="stat mr-1 border-right">
+                <div className="col-12 title text-muted text-center">OPEN</div>
+                <div className="no-gutters col-12 text-center value">{open}</div>
+            </div>
+            <div className="stat mr-1 border-right">
+                <div className="col-12 title text-muted text-center">HIGH</div>
+                <div className="no-gutters col-12 text-center value">{high}</div>
+            </div>
+            <div className="stat mr-1 border-right">
+                <div className="col-12 title text-muted text-center">LOW</div>
+                <div className="no-gutters col-12 text-center value">{low}</div>
+            </div>
+            <div className="stat mr-4">
+                <div className="col-12 title text-muted text-center">TREND</div>
+                <div className={`no-gutters col-12 text-center value 
+                ${trendValue > 0 ? 'negative' : 'positive'}`}>{`${trendValue > 0 ? '-' : ''}${trendValue} (${trendPercentage}%)`}</div>
             </div>
             {prop.data && <Button title={title()} 
                                   classes={prop.trackerMode}
