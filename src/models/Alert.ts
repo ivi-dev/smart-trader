d8985ec93f127c271911b79f09477766393795a4
Iliyan Videv <videviliyan@gmail.com>
2020-02-23 23:10:32 +0200

    Fixed alignment

--- src/models/Alert.ts
+++ src/models/Alert.ts
@@ -2,5 +2,5 @@ export type AlertLevel = 'all' | 'error' | 'warning' | 'info';
 
 export default class Alert {
     constructor(public id: number, public text: string, 
-        public level: AlertLevel = 'info') {}
+                public level: AlertLevel = 'info') {}
 }
